79e3329eb96d709757774e2e123298c9e4aa25a7
ritsathya <pisithritsathya@gmail.com>
2022-05-16 21:54:32 +0700

    Create Post view

---
+++ resources/views/post/store.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="flex justify-center w-full">
+      <div class="bg-white border-2 w-8/12 rounded-lg p-4 mt-4">
+
+      </div>
+    </div>
+@endsection
