4ac1b22114640356d667045cc791dacc7f1d8664
ritsathya <pisithritsathya@gmail.com>
2022-05-18 16:10:52 +0700

    Developing dashboard page

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Auth\Dashboard\DashboardController;
+use App\Http\Controllers\Auth\Dashboard\SliderController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
@@ -19,6 +21,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('layouts.app');
 });
+
+Route::get('/dashboard', function () {
+    return view('layouts.dashboard');
+})->name('dashboard');
+
+Route::get('/dashboard/index', [DashboardController::class, 'index'])->name('dashboard.index');
+Route::get('/dashboard/slider', [SliderController::class, 'index'])->name('dashboard.slider');
 
 Route::get('/register', [RegisterController::class, 'index'])->name('register');
 
