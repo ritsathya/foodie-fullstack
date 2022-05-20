552bab440fa270ff7597a977a57d481d36859665
ritsathya <pisithritsathya@gmail.com>
2022-05-20 14:18:20 +0700

    Refine slider-creation page

--- resources/views/dashboard/slider/create.blade.php
+++ resources/views/dashboard/slider/create.blade.php
@@ -81,10 +81,9 @@
               ease-in-out
               m-0
               focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" aria-label="Default select example" name="status" id="status">
-                <option selected>Open this select menu</option>
-                <option value="1">One</option>
-                <option value="2">Two</option>
-                <option value="3">Three</option>
+                <option disabled selected>Choose image status</option>
+                <option value="active">Active</option>
+                <option value="inactive">Inactive</option>
             </select>
           </div>
           <button
