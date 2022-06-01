1de74e87b6d105059bb7cfa433c2b96ac81e8da3
ritsathya <pisithritsathya@gmail.com>
2022-06-01 16:14:28 +0700

    Redesign rating and review input

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -9,58 +9,31 @@
         <x-posts.ingredient :ingredients="$ingredients"/>
         <x-posts.direction :directions="$directions"/>
 
-        <div class="flex justify-start space-x-2 p-4 mt-4">
-          <div class="shrink-0 w-6/12">
-            <div class="flex items-center mb-3">
-              <svg class="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
-              <svg class="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
-              <svg class="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
-              <svg class="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
-              <svg class="w-5 h-5 text-gray-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
-              <p class="ml-2 text-sm font-medium text-gray-900">4.95 out of 5</p>
+        <div class="bg-white p-8 mt-8 rounded shadow">
+          <form action="" method="POST" class="flex flex-col items-center space-y-4">
+            @csrf
+            <div>
+              <p class="text-center text-xl font-semibold">Rate this post</p>
+              <div class="rating-css">
+                <div class="star-icon">
+                    <input type="radio" value="1" name="post_rating" checked id="rating1">
+                    <label for="rating1" class="fas fa-star"></label>
+                    <input type="radio" value="2" name="post_rating" id="rating2">
+                    <label for="rating2" class="fas fa-star"></label>
+                    <input type="radio" value="3" name="post_rating" id="rating3">
+                    <label for="rating3" class="fas fa-star"></label>
+                    <input type="radio" value="4" name="post_rating" id="rating4">
+                    <label for="rating4" class="fas fa-star"></label>
+                    <input type="radio" value="5" name="post_rating" id="rating5">
+                    <label for="rating5" class="fas fa-star"></label>
+                </div>
             </div>
-            <p class="text-sm font-medium text-gray-500">1,745 global ratings</p>
-            <div class="flex items-center mt-4">
-              <span class="text-sm font-medium text-blue-600">5 <i class="fas fa-star text-yellow-400"></i></span>
-              <div class="w-2/4 h-3 mx-4 bg-gray-200 rounded-full">
-                <div class="h-3 bg-yellow-400 rounded-full" style="width: 70%"></div>
-              </div>
-              <span class="text-sm font-medium text-blue-600">70%</span>
             </div>
-            <div class="flex items-center mt-4">
-              <span class="text-sm font-medium text-blue-600">4 <i class="fas fa-star text-yellow-400"></i></span>
-              <div class="w-2/4 h-3 mx-4 bg-gray-200 rounded-full">
-                <div class="h-3 bg-yellow-400 rounded-full" style="width: 17%"></div>
-              </div>
-              <span class="text-sm font-medium text-blue-600">17%</span>
-            </div>
-            <div class="flex items-center mt-4">
-              <span class="text-sm font-medium text-blue-600">3 <i class="fas fa-star text-yellow-400"></i></span>
-              <div class="w-2/4 h-3 mx-4 bg-gray-200 rounded-full">
-                <div class="h-3 bg-yellow-400 rounded-full" style="width: 8%"></div>
-              </div>
-              <span class="text-sm font-medium text-blue-600">8%</span>
-            </div>
-            <div class="flex items-center mt-4">
-              <span class="text-sm font-medium text-blue-600">2 <i class="fas fa-star text-yellow-400"></i></span>
-              <div class="w-2/4 h-3 mx-4 bg-gray-200 rounded-full">
-                <div class="h-3 bg-yellow-400 rounded-full" style="width: 4%"></div>
-              </div>
-              <span class="text-sm font-medium text-blue-600">4%</span>
-            </div>
-            <div class="flex items-center mt-4">
-              <span class="text-sm font-medium text-blue-600">1 <i class="fas fa-star text-yellow-400 ml-1"></i></span>
-              <div class="w-2/4 h-3 mx-4 bg-gray-200 rounded-full">
-                <div class="h-3 bg-yellow-400 rounded-full" style="width: 2%"></div>
-              </div>
-              <span class="text-sm font-medium text-blue-600">1%</span>
-            </div>
-            <div class="mt-8 w-6/12">
-              <a href="#reivew" class="px-4 py-2 bg-gray-200 rounded-md shadow-md w-full">
-                Write your review
-              </a>
-            </div>
-          </div>
+            <textarea name="review" id="review" cols="30" rows="5" class="border-2 p-2 w-full rounded"></textarea>
+            <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Submit Review</button>
+          </form>
+        </div>
+        <div class="p-4 mt-4">
           <div id="commentSection">
             <div class="mb-4">
               <div class="flex items-center mb-4 space-x-4">
